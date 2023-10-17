853866bf145f7769455b50ff0e044673f56c8f89
Aaron Flood <aaronjflood@gmail.com>
2023-10-17 14:11:30 -0500

    REvision 1 complete for metals.

---
+++ Metals_submission_R1/buildPDF.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+filename=Flood_Appliation_of_Tuning_to_material_properties
+clean_filename=${filename}_clean
+sed 's/% final/final/g' ${filename}.tex > ${clean_filename}.tex
+
+if [[ "$(docker images -q latex)" == "" ]]; then
+	echo "Building the latex image"
+	docker build -t latex .
+fi
+
+for file in $filename $clean_filename
+do 
+	echo "Processing file: " $file
+
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
+
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			bibtex $file
+
+
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
+
+
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
+
+
+	rm -f $file.aux
+	rm -f $file.bbl
+	rm -f $file.blg
+	rm -f $file.log
+	rm -f $file.out
+done
--- buildPDF.sh
+++ buildPDF.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 
 filename=Flood_Appliation_of_Tuning_to_material_properties
-
+clean_filename=${filename}_clean
+sed 's/% final/final/g' ${filename}.tex > ${clean_filename}.tex
 
 if [[ "$(docker images -q latex)" == "" ]]; then
 	echo "Building the latex image"
 	docker build -t latex .
 fi
 
-docker run \
-		--user=$(id -u) \
-		--volume="/etc/group:/etc/group:ro" \
-		--volume="/etc/passwd:/etc/passwd:ro" \
-		--volume="/etc/shadow:/etc/shadow:ro" \
-		--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-		-v "$(pwd)":/data \
-		latex \
-		pdflatex $filename.tex
+for file in $filename $clean_filename
+do 
+	echo "Processing file: " $file
 
-docker run \
-		--user=$(id -u) \
-		--volume="/etc/group:/etc/group:ro" \
-		--volume="/etc/passwd:/etc/passwd:ro" \
-		--volume="/etc/shadow:/etc/shadow:ro" \
-		--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-		-v "$(pwd)":/data \
-		latex \
-		bibtex $filename
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
 
-
-docker run \
-		--user=$(id -u) \
-		--volume="/etc/group:/etc/group:ro" \
-		--volume="/etc/passwd:/etc/passwd:ro" \
-		--volume="/etc/shadow:/etc/shadow:ro" \
-		--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-		-v "$(pwd)":/data \
-		latex \
-		pdflatex $filename.tex
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			bibtex $file
 
 
-docker run \
-		--user=$(id -u) \
-		--volume="/etc/group:/etc/group:ro" \
-		--volume="/etc/passwd:/etc/passwd:ro" \
-		--volume="/etc/shadow:/etc/shadow:ro" \
-		--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-		-v "$(pwd)":/data \
-		latex \
-		pdflatex $filename.tex
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
 
 
-rm -f $filename.aux
-rm -f $filename.bbl
-rm -f $filename.blg
-rm -f $filename.log
-rm -f $filename.out
+	docker run \
+			--user=$(id -u) \
+			--volume="/etc/group:/etc/group:ro" \
+			--volume="/etc/passwd:/etc/passwd:ro" \
+			--volume="/etc/shadow:/etc/shadow:ro" \
+			--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+			-v "$(pwd)":/data \
+			latex \
+			pdflatex $file.tex
+
+
+	rm -f $file.aux
+	rm -f $file.bbl
+	rm -f $file.blg
+	rm -f $file.log
+	rm -f $file.out
+done
